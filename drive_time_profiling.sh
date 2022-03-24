ba20f303906bd279e0950bab18e74bf4df3f96bd
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2022-03-24 19:23:06 +0100

    Refactor so that two different problems can be run

--- drive_time_profiling.sh
+++ drive_time_profiling.sh
@@ -1,12 +1,38 @@
 #!/bin/bash
 
-MYDIR=$HOME/firedrakeexamples/profiletheprofiling/
-FDDIR=$HOME/firedrakeinstalls/fd060322/firedrake/
+# choose which problem to profile
+PROBLEM="problem2"
 
+# set working repo automatically, but you need to set the path to your firedrake install
+MYDIR=$HOME/firedrakeexamples/profiletheprofiling/"$PROBLEM"/
+mkdir $MYDIR
+FDDIR=$HOME/firedrakeinstalls/fd060322/firedrake/
 source "$FDDIR"bin/activate
-cd "$FDDIR"src/PyOP2
-git fetch sv/log-LACallables
-git checkout sv/log-LACallables
+
+# setups for the different problems
+if [ "$PROBLEM" = "problem1" ]; then
+    # problem1 profile only the profiling of the solve and inverse callables in PyOP2
+    BASE_COMMIT=7a6893414d9311150cddcc7c65ffda44b1fc5416
+    PERFORM_COMMIT=sv/log-LACallables
+    cd "$FDDIR"src/PyOP2
+    git fetch $PERFORM_COMMIT
+    git checkout $PERFORM_COMMIT
+else
+    # problem2 profiles also the profiling of all local kernels (e.g. forms for TSFC)
+    # so the corresponding commits in firedrake and tsfc also have to be updated
+    # if code is force pushed to the branches the base commits have to be updated
+    BASE_COMMITS_PTF=(676c3e72e66105ec2943af47b303df7ef6790c33  a4212334008c325a4daeb97c93e89946705717f9 4560f9f017cc01a11cc50564d108b0d67aca137b)
+    PERFORM_COMMIT=sv/profiling-local-kernels-trackevents
+    cd "$FDDIR"src/PyOP2
+    git fetch origin $PERFORM_COMMIT
+    git checkout $PERFORM_COMMIT
+    cd "$FDDIR"src/firedrake
+    git fetch origin $PERFORM_COMMIT
+    git checkout $PERFORM_COMMIT 
+    cd "$FDDIR"src/tsfc
+    git fetch origin $PERFORM_COMMIT
+    git checkout $PERFORM_COMMIT 
+fi
 
 # Take care, rerunning will remove previous results
 rm "$MYDIR"time_*.txt
@@ -14,30 +40,54 @@ rm "$MYDIR"results.png
 rm "$MYDIR"flame*.txt
 
 # Run timings on commit which does not allocate ids from python
-git checkout 7a6893414d9311150cddcc7c65ffda44b1fc5416
-for l in $(seq 20)
+if [ "$PROBLEM" = "problem1" ]; then
+    git checkout $BASE_COMMIT
+else
+    cd "$FDDIR"src/PyOP2
+    git checkout "${BASE_COMMITS_PTF[0]}"
+    cd "$FDDIR"src/tsfc
+    git checkout "${BASE_COMMITS_PTF[1]}"
+    cd "$FDDIR"src/firedrake
+    git checkout "${BASE_COMMITS_PTF[2]}"
+fi
+
+for l in $(seq 10)
 do
     firedrake-clean
-    python3 $MYDIR/time_profiling.py "old" $MYDIR "compile"
-    python3 $MYDIR/time_profiling.py "old" $MYDIR "compute"
+    python3 $MYDIR../time_profiling.py "old" $MYDIR "compile" $PROBLEM
+    python3 $MYDIR../time_profiling.py "old" $MYDIR "compute" $PROBLEM
 
     firedrake-clean
-    python3 $MYDIR/time_profiling.py "old" $MYDIR "compile" -log_view :$MYDIR/flame_setidfromC_compile.txt:ascii_flamegraph
-    python3 $MYDIR/time_profiling.py "old" $MYDIR "compute" -log_view :$MYDIR/flame_setidfromC_compute.txt:ascii_flamegraph
+    python3 $MYDIR../time_profiling.py "old" $MYDIR "compile" $PROBLEM -log_view :$MYDIR/flame_setidfromC_compile.txt:ascii_flamegraph
+    python3 $MYDIR../time_profiling.py "old" $MYDIR "compute" $PROBLEM -log_view :$MYDIR/flame_setidfromC_compute.txt:ascii_flamegraph
 done
 
 # Run timings on commit which allocate ids from python
-git checkout sv/log-LACallables
-for l in $(seq 20)
+if [ "$PROBLEM" = "problem1" ]; then
+    cd "$FDDIR"src/PyOP2
+    git checkout $PERFORM_COMMIT
+    cd "$FDDIR"src/tsfc
+    git checkout master
+    cd "$FDDIR"src/firedrake
+    git checkout master
+else
+    cd "$FDDIR"src/PyOP2
+    git checkout $PERFORM_COMMIT
+    cd "$FDDIR"src/tsfc
+    git checkout $PERFORM_COMMIT
+    cd "$FDDIR"src/firedrake
+    git checkout $PERFORM_COMMIT
+fi
+for l in $(seq 10)
 do
     firedrake-clean
-    python3 $MYDIR/time_profiling.py "new" $MYDIR "compile"
-    python3 $MYDIR/time_profiling.py "new" $MYDIR "compute"
+    python3 $MYDIR../time_profiling.py "new" $MYDIR "compile" $PROBLEM
+    python3 $MYDIR../time_profiling.py "new" $MYDIR "compute" $PROBLEM
 
     firedrake-clean
-    python3 $MYDIR/time_profiling.py "new" $MYDIR "compile" -log_view :$MYDIR/flame_setidfrompython_compile.txt:ascii_flamegraph
-    python3 $MYDIR/time_profiling.py "new" $MYDIR "compute" -log_view :$MYDIR/flame_setidfrompython_compute.txt:ascii_flamegraph
+    python3 $MYDIR../time_profiling.py "new" $MYDIR "compile" $PROBLEM -log_view :$MYDIR/flame_setidfrompython_compile.txt:ascii_flamegraph
+    python3 $MYDIR../time_profiling.py "new" $MYDIR "compute" $PROBLEM -log_view :$MYDIR/flame_setidfrompython_compute.txt:ascii_flamegraph
 done
 
 # gather data
-python3 $MYDIR/vis_results_time_profiling.py $MYDIR
+python3 $MYDIR../vis_results_time_profiling.py $MYDIR
--- time_profiling.py
+++ time_profiling.py
@@ -1,15 +1,17 @@
 from firedrake import *
 from firedrake.petsc import PETSc
 import time
-from os.path import exists
 import sys
 
 
+commit = sys.argv[1]
 path_to_dir = sys.argv[2]
 run = sys.argv[3]
+problem = sys.argv[4]
 
   
-def problem():
+def problem1():
+    """ Solve equations for lowest order RT discretisation of Poisson problem with Hybridization and CG+jacobi on the Trace system"""
     mesh = UnitSquareMesh(20, 20)
     U = FunctionSpace(mesh, "RT", 1)
     V = FunctionSpace(mesh, "DG", 0)
@@ -44,7 +46,115 @@ def problem():
     solve(a == L, w, bcs=bcs, solver_parameters=params)
 
 
-def timing(path_to_dir):
+def problem2():
+    """ Solve equations for lowest order RT discretisation of Poisson problem with Hybridization and CG+GTMG on the Trace system.
+        This will produce more local kernels than problem1. """
+    nlevels = 2
+    n=2
+    s=1
+    base = SquareMesh(n, n, s, quadrilateral=True)
+    basemh = MeshHierarchy(base, nlevels)
+    mh = ExtrudedMeshHierarchy(basemh, s, base_layer=n)
+    mesh = mh[-1]
+    x = SpatialCoordinate(mesh)
+
+    def get_p1_space():
+        return FunctionSpace(mesh, "CG", 1)
+
+    def get_p1_prb_bcs():
+        return [DirichletBC(get_p1_space(), zero(), "on_boundary"),
+                DirichletBC(get_p1_space(), zero(), "top"),
+                DirichletBC(get_p1_space(), zero(), "bottom")]
+
+    def p1_callback():
+        P1 = get_p1_space()
+        p = TrialFunction(P1)
+        q = TestFunction(P1)
+        return inner(grad(p), grad(q))*dx
+
+    p = 0
+    RT = FiniteElement("RTCF", quadrilateral, p+1)
+    DG_v = FiniteElement("DG", interval, p)
+    DG_h = FiniteElement("DQ", quadrilateral, p)
+    CG = FiniteElement("CG", interval, p+1)
+    HDiv_ele = EnrichedElement(HDiv(TensorProductElement(RT, DG_v)),
+                            HDiv(TensorProductElement(DG_h, CG)))
+    U = FunctionSpace(mesh, HDiv_ele)
+    V = FunctionSpace(mesh, "DQ", p)
+    W = U * V
+    sigma, u = TrialFunctions(W)
+    tau, v = TestFunctions(W)
+
+    L=1
+    exact = 100*x[0]*(L-x[0])*x[1]*(L-x[1])*x[2]*(L-x[2])
+    f = -div(grad(exact))
+
+    a = (dot(sigma, tau) + div(tau)*u + div(sigma)*v)*dx
+    L = inner(f, v)*dx
+
+    w = Function(W)
+    
+    mgmatfree_mtf = {'snes_type': 'ksponly',
+                    'ksp_type': 'preonly',
+                    'mat_type': 'matfree',
+                    'pc_type': 'mg',
+                    'pc_mg_type': 'full',
+                    'ksp_rtol': 1e-8,
+                    'mg_coarse_ksp_type': 'preonly',
+                    'mg_coarse_pc_type': 'python',
+                    'mg_coarse_pc_python_type': 'firedrake.AssembledPC',
+                    'mg_coarse_assembled_pc_type': 'lu',
+                    'mg_coarse_assembled_pc_factor_mat_solver_type': 'superlu_dist',
+                    'mg_levels_ksp_type': 'chebyshev',
+                    'mg_levels_ksp_norm_type': 'unpreconditioned',
+                    'mg_levels_ksp_max_it': 3,
+                    'mg_levels_pc_type': 'none',
+                    'ksp_norm_type': 'preconditioned'
+                    }
+
+    params = {'mat_type': 'matfree',
+                        'ksp_type': 'preonly',
+                        'pc_type': 'python',
+                        'pc_python_type': 'firedrake.HybridizationPC',
+                        'hybridization': {'ksp_type': 'cg',
+                                            'pc_type': 'python',
+                                            'ksp_rtol': 1e-5,
+                                            'ksp_atol': 1e-8,
+                                            'mat_type': 'matfree',
+                                            'localsolve': {'ksp_type': 'preonly',
+                                                            'pc_type': 'fieldsplit',
+                                                            'pc_fieldsplit_type': 'schur'},
+                                            'ksp_norm_type': 'preconditioned',
+                                            'pc_python_type': 'firedrake.GTMGPC',
+                                            'gt': {'mg_levels': {'ksp_type': 'chebyshev',
+                                                                 'pc_type': 'none',
+                                                                 'ksp_max_it': 3},
+                                                    'mg_coarse': mgmatfree_mtf,
+                                                    'mat_type':'matfree'
+                                                    }
+                                            }
+                        }
+    appctx = {'get_coarse_operator': p1_callback,
+                'get_coarse_space': get_p1_space,
+                'coarse_space_bcs': get_p1_prb_bcs()}
+
+
+    if ("hybridization" in params.keys() 
+        and "localsolve" in params["hybridization"].keys() 
+        and "mat_type" in params["hybridization"]["localsolve"].keys()):
+        if params["hybridization"]["localsolve"]["mat_type"] == "matfree":
+            fcp = {"slate_compiler": {"replace_mul": True}}
+            appctx["form_compiler_parameters"] = fcp
+    else:
+        fcp = {}
+
+    solve(a == L, w, solver_parameters=params, appctx=appctx, form_compiler_parameters=fcp)
+
+def timing(path_to_dir, problem):
+    if problem == "problem1":
+        problem = problem1
+    elif problem == "problem2":
+        problem = problem2
     start_time = time.time()
     problem()
     timed = (time.time() - start_time)
@@ -53,7 +163,6 @@ def timing(path_to_dir):
         myfile.write(str(timed)+",")
     
 
-commit = sys.argv[1]
 log = PETSc.Log.isActive()
 
-timing(f"{path_to_dir}time_log{log}_{run}_{commit}.txt")
+timing(f"{path_to_dir}time_log{log}_{run}_{commit}.txt", problem)
